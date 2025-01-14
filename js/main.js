ab6d8d817b8dc320229ef8acb14406429363311d
Federico Rizzardi <federico.rizzardi@marconirovereto.it>
2025-01-14 09:32:43 +0100

    aggiunto <li> tre

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-console.log("main.js");
-function ciao(messaggio) {
-    console.log(`Ciao ${messaggio}`);
-    
-}
-ciao('mondo')
+const lista = document.querySelector('#lista');
+
+const nuovoElemento = document.createElement('li');
+
+nuovoElemento.textContent = 'Tre';
+
+console.log(nuovoElemento);
+
+lista.appendChild(nuovoElemento);
