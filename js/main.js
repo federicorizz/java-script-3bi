e0942c2834efc6c492bf8bef297dc82175a6412b
Federico Rizzardi <federico.rizzardi@marconirovereto.it>
2025-01-21 08:27:26 +0100

    query selectors pt.1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const paragrafo = document.querySelector('p');
+
+console.log(paragrafo);
+
+
+
+
 const lista = document.querySelector('#lista');
 
 const nuovoElemento = document.createElement('li');
