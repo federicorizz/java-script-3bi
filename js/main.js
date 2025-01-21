db8a113e21d1b404cd7d9c00bc776e39aad4aa46
Federico Rizzardi <federico.rizzardi@marconirovereto.it>
2025-01-21 08:32:58 +0100

    manipolare una classe

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const $$ = document.querySelectorAll.bind(document);
 
 const paragrafo = document.querySelector('p');
 
+paragrafo.classList.add('red');
+
 console.log(paragrafo);
 
 const paragrafi = document.querySelectorAll('p');
